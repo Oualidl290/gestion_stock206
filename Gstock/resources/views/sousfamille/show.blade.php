22f856fe4f76105d5bb7194338deadece77be3e8
Lemsidi <164000364+Lemsidi@users.noreply.github.com>
2024-03-30 23:57:39 +0100

    Create show.blade.php

---
+++ Gstock/resources/views/sousfamille/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('content')
+ 
+ 
+<div class="card">
+  <div class="card-header"> Page sousfamilles</div>
+  <div class="card-body">
+   
+ 
+        <div class="card-body">
+        <h5 class="card-title">sousfamilles : {{ $sousfamilles->sousfamilles}}</h5>
+        
+  </div>
+       
+    </hr>
+  
+  </div>
+</div>
+@endsection
