aea7354cac9b0b9549647cf83091ce2b55178806
Lemsidi <164000364+Lemsidi@users.noreply.github.com>
2024-03-31 00:20:38 +0100

    Update Sousfamille.php

--- Gstock/app/Models/Sousfamille.php
+++ Gstock/app/Models/Sousfamille.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sousfamille extends Model
+class sousfamille extends Model
 {
+
+    protected $table = 'sousfamilles';
+    protected $primaryKey = 'id';
+    protected $fillable = ['sousfamilles'];
+
     use HasFactory;
-}
+}?>
